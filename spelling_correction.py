00d595ce7d6cea2b3490ab75d3e05b2dc67e0ed8
Unknown <barrett370@gmail.com>
2018-10-05 20:37:56 +0100

    completed spell check ex.

--- spelling_correction.py
+++ spelling_correction.py
@@ -8,7 +8,7 @@ def words(text): return re.findall(r'\w+', text.lower())
 WORDS = Counter(words(open('data/big.txt').read()))
 
 
-def denom(dictionary):
+def denominator(dictionary):
     acc = 0
     for value in dictionary.values():
         acc += value
@@ -16,7 +16,7 @@ def denom(dictionary):
 
 
 def P(word):
-    return WORDS[word] / denom(WORDS)
+    return WORDS[word] / denominator(WORDS)
 
 
 print(P('the'))
@@ -34,7 +34,13 @@ def edits1(word):
             temp2 = list(word)
             temp2.insert(i, chr(j))
             permutations.append("".join(temp2))
-
+        for j in range(len(word)):
+            if i != j:
+                temp5 = list(word)
+                t = temp5[i]
+                temp5[i] = temp5[j]
+                temp5[j] = t
+                permutations.append("".join(temp5))
         if i <= len(word):
             temp3 = list(word)
             temp3[i] = ""
@@ -43,17 +49,69 @@ def edits1(word):
         temp4 = list(word)
         temp4.append(chr(j))
         permutations.append("".join(temp4))
-    temp = list(word)
-    for i in range(len(temp)):
-        for j in range(len(temp)):
-            if i != j:
-                t = temp[i]
-                temp[i] = temp[j]
-                temp[j] = t
-                permutations.append("".join(temp))
 
     return permutations
 
 
 print(len(edits1('cat')))
 print(edits1('cat'))
+
+
+def edits2(word):
+    permutations = edits1(word)
+    ret = []
+    for each in permutations:
+        ret += edits1(each)
+
+    return ret
+
+
+print(edits2('cat'))
+print(len(edits2('cat')))
+
+
+def rem_dup(words):
+    acc = []
+    for word in words:
+        if not acc.__contains__(word):
+            acc.append(word)
+    return acc
+
+
+print(len(rem_dup(edits1('cat'))))
+print(rem_dup(edits1('cat')))
+
+
+def check_dict(words):
+    acc = []
+    for word in words:
+        if WORDS[word] != 0:
+            acc.append(word)
+    return acc
+
+
+print(check_dict(edits1('cat')))
+
+
+def candidates(word):
+    corrections = []
+    corrections += edits1(word)
+    corrections += edits2(word)
+    # print(len(corrections))
+    corrections2 = check_dict(corrections)
+    # print(len(corrections2))
+    return corrections2
+
+
+print("\n\n\n")
+print(candidates('ca'))
+print("CanLEN"+ str(len(candidates('cat'))))
+
+def predict(word):
+    possible_corrections = candidates(word)
+    possible_corrections.sort(key=lambda x: P(x), reverse=True)
+    return possible_corrections[0]
+
+
+print(predict('cimputer'))
+print(P('ba'))
